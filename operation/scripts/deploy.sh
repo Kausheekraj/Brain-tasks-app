e615be05d60283ea379f4b9cbe5f68e5f1d6f33a
Kausheekraj <kausheek94@gmail.com>
2025-12-24 16:24:21 +0530

    New deploy script

--- operation/scripts/deploy.sh
+++ operation/scripts/deploy.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
-# Stopping any existing containers
-echo " Stopping any existing containers"
-docker compose down 
-echo "Deploying New Nginx Container..."
-docker compose up --build -d
+set -e
+echo "Stopping any existing containers of this image..."
+kubectl delete -f deployment.yaml --ignore-not-found
+kubectl delete -f service.yaml   --ignore-not-found  
+kubectl delete -f hpa.yaml     --ignore-not-found
+echo "Deploying Container..."
+
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml
+kubectl apply -f hpa.yaml
+
